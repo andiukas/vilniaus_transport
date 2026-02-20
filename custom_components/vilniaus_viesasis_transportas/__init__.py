b4ba798bac41caf9b6041bcba63b15cf7900dfa3
andiukas <44068098+andiukas@users.noreply.github.com>
2026-02-20 10:48:50 +0200

    Update __init__.py

--- custom_components/vilniaus_viesasis_transportas/__init__.py
+++ custom_components/vilniaus_viesasis_transportas/__init__.py
@@ -3,21 +3,19 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-_LOGGER = logging.getLogger(__name__)
-
-# TURI sutapti su manifest.json "domain"
+# TURI SUTAPTI SU MANIFEST
 DOMAIN = "vilniaus_viesasis_transportas"
-PLATFORMS = ["device_tracker"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Nustatoma integracija iš Config Entry."""
-    # Ši eilutė perduoda valdymą į device_tracker.py
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    """Nustatome integraciją."""
+    # Ši eilutė liepia HA užkrauti device_tracker.py
+    await hass.config_entries.async_forward_entry_setups(entry, ["device_tracker"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Ištrinama integracija (kai vartotojas paspaudžia Delete)."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    """Ištriname integraciją."""
+    return await hass.config_entries.async_unload_platforms(entry, ["device_tracker"])
+
 
 
 
