3c9196aa4eb6be54afa56f59b481bd3e37952efe
andiukas <44068098+andiukas@users.noreply.github.com>
2026-02-20 10:38:27 +0200

    Update __init__.py

--- custom_components/vilniaus_viesasis_transportas/__init__.py
+++ custom_components/vilniaus_viesasis_transportas/__init__.py
@@ -1,26 +1,24 @@
 """Vilniaus viešasis transportas integracija."""
 import logging
-from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
 _LOGGER = logging.getLogger(__name__)
 
+# TURI sutapti su manifest.json "domain"
 DOMAIN = "vilniaus_viesasis_transportas"
 PLATFORMS = ["device_tracker"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Nustatoma integracija iš Config Entry."""
-    _LOGGER.info("Kraunama Vilniaus transporto integracija maršrutui: %s", entry.data.get("route"))
-    
-    # Ši eilutė užregistruoja platformas (mūsų atveju device_tracker)
+    # Ši eilutė perduoda valdymą į device_tracker.py
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Ištrinama integracija."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
-    return unload_ok
+    """Ištrinama integracija (kai vartotojas paspaudžia Delete)."""
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
 
 
 
