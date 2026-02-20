1745bff7aad5059639da1a79fdf7d30b4fcc10bd
andiukas <44068098+andiukas@users.noreply.github.com>
2026-02-20 10:30:28 +0200

    Update __init__.py

--- custom_components/vilniaus_viesasis_transportas/__init__.py
+++ custom_components/vilniaus_viesasis_transportas/__init__.py
@@ -1,18 +1,26 @@
 """Vilniaus viešasis transportas integracija."""
+import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+
+_LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "vilniaus_viesasis_transportas"
 PLATFORMS = ["device_tracker"]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up Vilnius Transport from a config entry."""
+    """Nustatoma integracija iš Config Entry."""
+    _LOGGER.info("Kraunama Vilniaus transporto integracija maršrutui: %s", entry.data.get("route"))
+    
+    # Ši eilutė užregistruoja platformas (mūsų atveju device_tracker)
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    """Ištrinama integracija."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    return unload_ok
 
 
 
